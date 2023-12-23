8fb50723c7fe3b275ee1766bc9a7828cac25ea08
Tzz <tangbe9@gmail.com>
2023-12-23 17:38:38 +0000

    bup

--- .sandbox/auto_scale.py
+++ .sandbox/auto_scale.py
@@ -9,7 +9,7 @@ import datetime
 import functools
 
 bf_server_metrics_url="http://bf-server:9090/metrics"
-scale_duration = 20
+scale_duration = 10
 min_worker = 1
 max_worker = 4
 
@@ -18,7 +18,7 @@ class Global:
     queue_empty_start_time = 0
     expected_worker_num = 1
     is_scale_up_trending = True
-    trending_start_time = None
+    trending_start_time = datetime.datetime.now()
     def __init__(self):
         self.sandbox_name = os.getenv("SANDBOX_NAME")
         if self.sandbox_name == "" or self.sandbox_name == None:
@@ -30,6 +30,7 @@ class BuildfarmServerMetrics:
     worker_pool_size = 0 
 
 class BuildfarmWorkerMetrics:
+    worker_name = ""
     execution_slot_usage = 0
 
 
@@ -40,6 +41,7 @@ def main():
     while True:
         err = scale(g)
         if err != None:
+            log_error(str(err))
             time.sleep(2)
             continue
         time.sleep(5)
@@ -50,17 +52,21 @@ def scale(g):
     if err != None:
         log.error("failed to scrape buildfarm server metrics: " + str(err))
         return False
+    print("DEBUG {} {}".format(server_metrics.queue_size, server_metrics.worker_pool_size))
     now = datetime.datetime.now()
     if server_metrics.queue_size > 0:
         if not g.is_scale_up_trending:
-            g.is_scale_up_trending = true
+            log("queue_szie is {}, switching to scale up trending".format(server_metrics.queue_size))
+            g.is_scale_up_trending = True
             g.trending_start_time = now
     else:
         if g.is_scale_up_trending:
-            g.is_scale_up_trending = false
+            log("queue_szie is 0, switching to scale down trending")
+            g.is_scale_up_trending = False
             g.trending_start_time = now
     
     if (now - g.trending_start_time).total_seconds() >= scale_duration:
+        print("DEBUG reach scaling duration")
         if g.is_scale_up_trending and g.expected_worker_num <= server_metrics.worker_pool_size:
             g.expected_worker_num += 1
         if not g.is_scale_up_trending and g.expected_worker_num >= server_metrics.worker_pool_size:
@@ -70,6 +76,7 @@ def scale(g):
             g.expected_worker_num = max_worker
         if g.expected_worker_num <  min_worker:
             g.expected_worker_num = min_worker
+        print("DEBUG expecte worker {}".format(g.expected_worker_num))
 
     if g.expected_worker_num > server_metrics.worker_pool_size:
         return scale_up(g,server_metrics)
@@ -81,51 +88,55 @@ def scale(g):
 
 
 def scale_up(g,server_metrics):
-    template,err = sandbox_template()
+    template,err = sandbox_template(g)
     if err != None:
         return err
-    workers_in_template,err = workers_defined_in_template(template)
-    if err != None:
-        return err
+    workers_in_template = workers_defined_in_template(template)
     scale_num = g.expected_worker_num - len(workers_in_template)  
     if scale_num <=  0:
         return
+    log("scaling up to {} workers, need to add {} worker".format(g.expected_worker_num, scale_num))
     for i in range(0,scale_num):
         alloc_worker_to_template(template)
-    result = subprocess.run("cs sandbox edit --from -",input=json.dumps(template),capture_output=True, text=True)
+    result = subprocess.run(["cs","sandbox","edit","-f","-"],input=json.dumps(template),capture_output=True, text=True)
     if result.returncode != 0:
         return result.stderr
     return None
 
 
 def scale_down(g,server_metrics):
-    template,err = sandbox_template()
+    print("DEBUG scale_down")
+    template,err = sandbox_template(g)
     if err != None:
-        return err
-    workers_in_template,err = workers_defined_in_template(template)
-    if err ! None:
-        return err
+        return log_error(str(err))
+    workers_in_template = workers_defined_in_template(template)
 
     scale_num =  len(workers_in_template)  - g.expected_worker_num 
     if scale_num <= 0:
         return
+
+    log("scaling down to {} workers, need to remove {} worker".format(g.expected_worker_num, scale_num))
     
-    metrics ={}
+    metrics =[]
     for i in range(0,scale_num):
         for worker in workers_in_template:
-            worker_metric,err = scrpae_buildfarm_worker_metrics("http://{}:9090/metric".format(worker.name))
+            worker_metric,err = scrpae_buildfarm_worker_metrics(worker["name"])
             if err == None:
-                metrics[worker.name] = worker_metric
+                metrics.append(worker_metric)
         idle_workers =list(filter(lambda x: x.execution_slot_usage ==0, metrics))
+        print("DEBUG idle_workers",idle_workers)
         if len(idle_workers) == 0:
             return None
-        template["containers"] = list(filter(lambda container : container.name != idle_workers[0].name,template["containers"] ))
-        result = subprocess.run("cs sandbox edit --from -",input=json.dumps(template),capture_output=True, text=True)
+        print("DEBUG container ",len(template["containers"]))
+        print(idle_workers[0].worker_name)
+        template["containers"] = list(filter(lambda container : container["name"] != idle_workers[0].worker_name,template["containers"] ))
+        print("DEBUG container ",len(template["containers"]))
+        result = subprocess.run(["cs","sandbox","edit","--from","-"],input=json.dumps(template),capture_output=True, text=True)
         if result.returncode != 0:
             return result.stderr
     return None
 
-def sandbox_template():
+def sandbox_template(g):
     result = subprocess.run(["cs","sandbox","show",g.sandbox_name,"-d","-o","json"],capture_output=True, text=True)
     if result.returncode != 0 :
         return None,result.stderr
@@ -134,23 +145,25 @@ def sandbox_template():
 
 
 def workers_defined_in_template(template):
-    return list(filter(lambda x: x.name.startWith("bf-worker"), template["containers"])), None
+    return list(filter(lambda container: container["name"].startswith("bf-worker"), template["containers"]))
 
 def alloc_worker_to_template(template):
+    workers = workers_defined_in_template(template)
     for ii in range (0,max_worker):
         worker_name = "bf-worker"+str(ii)
-        if not template_has_bf_worker(template,worker_name):
-            worker = template["containers"][0].copy()
+        if not has_bf_worker(workers,worker_name):
+            worker = workers[0].copy()
             worker["name"] = worker_name
             template["containers"].append(worker)
+            return
 
 
-def template_has_bf_worker(template,name):
-    for worker in workers_defined_in_template(template):
-        if worker.name == name:
-            return true
+def has_bf_worker(workers,name):
+    for worker in workers:
+        if worker["name"] == name:
+            return True
 
-    return false
+    return False
 
 
 def scrape_buildfarm_server_metrics():
@@ -161,23 +174,32 @@ def scrape_buildfarm_server_metrics():
     data,err = parse_json(result.stdout)
     if err != None:
         return None,err
-    server_metrics.queue_size = functools.reduce(
-        lambda a,b: a+b,
-        map(
-            lambda metric: functools.reduce(lambda a,b:a+b,map(lambda x: int(x["value"]),metric["metrics"])),
-            filter(lambda obj: obj["name"] == "queue_size", data),
-        ),
-    )
-    server_metrics.worker_pool_size = functools.reduce(
-        lambda a,b: a+b,
-        map(
-            lambda metric: functools.reduce(lambda a,b:a+b,map(lambda x: int(x["value"]),metric["metrics"])),
-            filter(lambda obj: obj["name"] == "worker_pool_size", data),
-        ),
-    )
 
-def scrpae_buildfarm_worker_metrics(url):
-    metrics = BuildfarmWorkerMetrics()
+    queue_size_metric = list(map(
+        lambda metric: functools.reduce(lambda a,b:a+b,map(lambda x: int(x["value"]),metric["metrics"])),
+        filter(lambda obj: obj["name"] == "queue_size", data),
+    ))
+    if len(queue_size_metric) > 0:
+        server_metrics.queue_size = functools.reduce(
+            lambda a,b: a+b,
+            queue_size_metric,
+        )
+    worker_pool_size_metric =list(map(
+        lambda metric: functools.reduce(lambda a,b:a+b,map(lambda x: int(x["value"]),metric["metrics"])),
+        filter(lambda obj: obj["name"] == "worker_pool_size", data),
+    ))
+    if len(worker_pool_size_metric) > 0:
+        server_metrics.worker_pool_size = functools.reduce(
+            lambda a,b: a+b,
+            worker_pool_size_metric,
+        )
+
+    return server_metrics,None
+
+def scrpae_buildfarm_worker_metrics(worker_name):
+    url = "http://{}:9090/metric".format(worker_name)
+    metrics = BuildfarmWorkerMetrics() 
+    metrics.worker_name = worker_name
     result = subprocess.run(["prom2json",url],capture_output=True, text=True)
     if result.returncode != 0 :
         return None,result.stderr
